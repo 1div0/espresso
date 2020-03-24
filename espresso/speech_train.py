07f5e819c813f2b4326ef5152d87a9d71ddb7202
freewym <freewym@gmail.com>
2020-03-24 16:01:19 -0400

    code adaptation/changes according to the commits on Mar 24-26, 2020; use data.encoders.{bpe,tokenizer} for subword decode

--- espresso/speech_train.py
+++ espresso/speech_train.py
@@ -21,6 +21,7 @@ from fairseq import checkpoint_utils, distributed_utils, options, tasks, utils
 from fairseq.data import iterators
 from fairseq.logging import meters, metrics, progress_bar
 from fairseq.trainer import Trainer
+from fairseq.model_parallel.megatron_trainer import MegatronTrainer
 
 
 logging.basicConfig(
@@ -37,6 +38,7 @@ def main(args, init_distributed=False):
 
     assert args.max_tokens is not None or args.max_sentences is not None, \
         'Must specify batch size either with --max-tokens or --max-sentences'
+    metrics.reset()
 
     # Initialize CUDA and distributed training
     if torch.cuda.is_available() and not args.cpu:
@@ -70,7 +72,11 @@ def main(args, init_distributed=False):
     ))
 
     # Build trainer
-    trainer = Trainer(args, task, model, criterion)
+    if args.model_parallel_size == 1:
+        trainer = Trainer(args, task, model, criterion)
+    else:
+        trainer = MegatronTrainer(args, task, model, criterion)
+
     logger.info('training on {} GPUs'.format(args.distributed_world_size))
     logger.info('max input frames per GPU = {} and max sentences per GPU = {}'.format(
         args.max_tokens,
@@ -298,10 +304,6 @@ def print_options_meaning_changes(args):
 
 def cli_main(modify_parser=None):
     parser = options.get_training_parser()
-    parser.add_argument('--remove-bpe', nargs='?', const='@@ ', default=None,
-                        help='remove BPE tokens before scoring '
-                        '(can be set to sentencepiece). Being used for monitoring '
-                        'and validation')
     args = options.parse_args_and_arch(parser, modify_parser=modify_parser)
     print_options_meaning_changes(args)
 
